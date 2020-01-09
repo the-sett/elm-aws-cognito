90b7bc2077c215cc355bd22bf04127aa71ac0e2e
Rupert Smith <rupert@thesett.com>
2020-01-09 17:16:01 +0000

    Copied example from auth-elm to use as a starting point for this.

---
+++ example/assets/index.js
@@ -0,0 +1,9 @@
+require('../assets/images/data_center-large.png')
+
+const {
+  Elm
+} = require('../src/elm/Top.elm');
+
+const app = Elm.Top.init({
+  node: document.getElementById('application')
+});
