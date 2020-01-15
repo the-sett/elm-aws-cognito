c3147ff9056f3b732ccba167c67ea3f43f5f8cb8
Rupert Smith <rupert@thesett.com>
2020-01-15 11:49:09 +0000

    Putting the demo in another project.

--- example/assets/index.js
+++
@@ -1,9 +0,0 @@
-require('../assets/images/data_center-large.png')
-
-const {
-  Elm
-} = require('../src/elm/Top.elm');
-
-const app = Elm.Top.init({
-  node: document.getElementById('application')
-});
